518b231b9fd1a4da13731916cf9c1b9823f36ee0
Aster <galaster@foxmail.com>
2025-04-03 04:35:54 +0800

    Share the allocator

--- projects/vks-compiler/tests/main.rs
+++ projects/vks-compiler/tests/main.rs
@@ -3,28 +3,32 @@ fn ready() {
     println!("it works!")
 }
 
-use std::{
-    fs::File,
-    io::Write,
-    path::{Path, PathBuf},
-};
-
 use oxc::{
     allocator::Allocator,
+    ast::ast::Program,
     codegen::{CodeGenerator, CodegenOptions, LegalComment},
     parser::Parser,
     semantic::SemanticBuilder,
     span::SourceType,
-    transformer::{HelperLoaderMode, TransformOptions, Transformer},
+    transformer::{DecoratorOptions, HelperLoaderMode, HelperLoaderOptions, TransformOptions, Transformer, TypeScriptOptions},
 };
+use oxc_isolated_declarations::{IsolatedDeclarations, IsolatedDeclarationsOptions};
+use std::{
+    borrow::Cow,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
+use vks_compiler::{VksError, VksErrorKind};
 
 #[test]
-fn main() {
+fn main() -> Result<(), VksError> {
     let here = Path::new(env!("CARGO_MANIFEST_DIR"));
     let compiler = CompileOptions { debug: true };
-    compiler.generate_file(&here.join("tests/basic/index.ts"), &here.join("tests/basic/debug"));
+    compiler.writer().generate_file(&here.join("tests/basic/index.ts"), &here.join("tests/basic/debug"))?;
     let compiler = CompileOptions { debug: false };
-    compiler.generate_file(&here.join("tests/basic/index.ts"), &here.join("tests/basic/release"))
+    compiler.writer().generate_file(&here.join("tests/basic/index.ts"), &here.join("tests/basic/release"))?;
+    Ok(())
 }
 
 pub struct CompileOptions {
@@ -32,22 +36,47 @@ pub struct CompileOptions {
 }
 
 impl CompileOptions {
-    fn generate_file(&self, input: &Path, output: &Path) {
-        if output.exists() {
-            if output.is_dir() {
-            }
-            else {
-                panic!("{} is not a directory", output.display())
-            }
+    pub fn writer(&self) -> CompileWriter {
+        CompileWriter { allocator: Allocator::default(), options: self.clone() }
+    }
+}
+
+pub struct CompileWriter<'i> {
+    allocator: Allocator,
+    options: &'i CompileOptions,
+}
+
+impl<'i> CompileWriter<'i> {
+    fn ensure_io(&self, input: &Path, output: &Path) -> Result<(), VksError> {
+        if input.is_file() {
         }
         else {
-            std::fs::create_dir_all(output).unwrap()
+            Err(VksErrorKind::UnknownError)?
         }
+        if output.exists() {
+            if output.is_dir() { Ok(()) } else { Err(VksErrorKind::UnknownError)? }
+        }
+        else {
+            match std::fs::create_dir_all(output) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(VksErrorKind::UnknownError)?,
+            }
+        }
+    }
+
+    fn generate_dts<'a>(&self, input: Program<'a>, output: &Path) -> Result<(), VksError> {
+        let id_ret =
+            IsolatedDeclarations::new(&self.allocator, IsolatedDeclarationsOptions { strip_internal: true }).build(&ret.program);
+        let printed = CodeGenerator::new().build(&id_ret.program).code;
+    }
+
+    fn generate_file(&self, input: &Path, output: &Path) -> Result<(), VksError> {
+        self.ensure_io(input, output)?;
+
         let source_text = std::fs::read_to_string(input).unwrap_or_else(|err| panic!("{} not found.\n{err}", input.display()));
-        let allocator = Allocator::default();
         let source_type = SourceType::from_path(input).unwrap();
 
-        let ret = Parser::new(&allocator, &source_text, source_type).parse();
+        let ret = Parser::new(&self.allocator, &source_text, source_type).parse();
 
         if !ret.errors.is_empty() {
             println!("Parser Errors:");
@@ -78,9 +107,27 @@ impl CompileOptions {
 
         let scoping = ret.semantic.into_scoping();
 
-        let mut transform_options = TransformOptions::default();
-        transform_options.helper_loader.mode = HelperLoaderMode::External;
-
+        let transform_options = TransformOptions {
+            cwd: Default::default(),
+            assumptions: Default::default(),
+            typescript: TypeScriptOptions {
+                jsx_pragma: Default::default(),
+                jsx_pragma_frag: Default::default(),
+                only_remove_type_imports: false,
+                allow_namespaces: true,
+                allow_declare_fields: true,
+                optimize_const_enums: true,
+                rewrite_import_extensions: None,
+            },
+            decorator: DecoratorOptions { legacy: false, emit_decorator_metadata: true },
+            jsx: Default::default(),
+            env: Default::default(),
+            proposals: Default::default(),
+            helper_loader: HelperLoaderOptions {
+                module_name: Cow::Borrowed("@valkyrie-language/vks-runtime"),
+                mode: HelperLoaderMode::External,
+            },
+        };
         let ret = Transformer::new(&allocator, input, &transform_options).build_with_scoping(scoping, &mut program);
 
         if !ret.errors.is_empty() {
@@ -91,7 +138,7 @@ impl CompileOptions {
             }
         }
 
-        let mut codegen = CodegenOptions {
+        let codegen = CodegenOptions {
             single_quote: true,
             minify: !self.debug,
             comments: !self.debug,
@@ -111,5 +158,6 @@ impl CompileOptions {
                 panic!("missing")
             }
         };
+        Ok(())
     }
 }
