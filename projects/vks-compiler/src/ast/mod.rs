05979f2f505d2d7fb92cb8939f9b67c8cd5b1e99
aster <192608617@qq.com>
2025-05-13 21:24:14 +0800

    Add algebraic effect operators

--- projects/vks-compiler/src/ast/mod.rs
+++ projects/vks-compiler/src/ast/mod.rs
@@ -2,10 +2,33 @@ use arcstr::ArcStr;
 use oxc_span::Span;
 use std::ops::Range;
 
+
+#[derive(Clone, Debug)]
 pub struct VikingProgram {
     statements: Vec<VikingStatement>,
     span: Span,
     file: ArcStr,
 }
+#[derive(Clone, Debug)]
+pub enum VikingStatement {
+    TryHandler(TryHandlerStatement),
+    Raise(RaiseStatement),
+    Resume(ResumeStatement),
+}
 
-pub enum VikingStatement {}
+#[derive(Clone, Debug)]
+pub struct TryHandlerStatement {
+    span: Span,
+    file: ArcStr,
+}
+#[derive(Clone, Debug)]
+pub struct RaiseStatement {
+    span: Span,
+    file: ArcStr,
+}
+
+#[derive(Clone, Debug)]
+pub struct ResumeStatement {
+    span: Span,
+    file: ArcStr,
+}
